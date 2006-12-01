1300dd905dca12cb678443c6b8b321f9a2dd5ba1
largo_winz <wintsu@gmail.com>
2006-12-01 22:53:43 +0000

    add Doxygen

--- ConstraintEditor/include/anchorController.h
+++ ConstraintEditor/include/anchorController.h
@@ -11,55 +11,75 @@ class Anchor;
 }
 using namespace hLib;
 
+/**
+ * Classe permettant la gestion des ancres sur la vue ARéVi
+ */
 class AnchorController : public ArObject {
 public :
   AR_CLASS(AnchorController)
   AR_CONSTRUCTOR(AnchorController)
-    //AR_CONSTRUCTOR_1(AnchorController,ArRef<UndoRedo>,undoRedo)
 
-  // Anchors management
+  /**
+   * 
+   */
+  virtual void postChanges(void);
 
-  virtual
-  void
-  postChanges(void);
+  /**
+   * Méthode permettant de retourner le nombre d'ancres du modèle chargé
+   * @return Le nombre d'ancres du modèle chargé
+   */
+  virtual size_t getNbAnchors(void) const;
 
-  virtual
-  size_t
-  getNbAnchors(void) const;
+  /**
+   * Méthode permettant d'ajouter une ancre au modèle chargé
+   * @param anchor L'ancre à ajouter
+   */
+  virtual void addAnchor(ArRef<Anchor> anchor);
 
-  virtual
-  void
-  addAnchor(ArRef<Anchor> anchor);
+  /**
+   * Méthode permettant d'enlever une ancre au modèle chargé
+   * @param anchor L'ancre à enlever
+   */
+  virtual void removeAnchor(ArRef<Anchor> anchor);
+  
+  /**
+   * Méthode permettant de supprimer toutes les ancres du modèle chargé
+   */
+  virtual void clearAnchors(void);
 
-  virtual
-  void
-  removeAnchor(ArRef<Anchor> anchor);
+  /**
+   * Méthode permettant de récupérer en consultation une ancre dont l'indice est passé en paramètre
+   * @param id L'indice de l'ancre
+   * @return L'ancre
+   */
+  virtual ArConstRef<Anchor> getAnchor(size_t id) const;
 
-  virtual
-  void
-  clearAnchors(void);
-
-  virtual
-  ArConstRef<Anchor>
-  getAnchor(size_t id) const;
-
-  virtual
-  ArRef<Anchor>
-  accessAnchor(size_t id);
+  /**
+   * Méthode permettant de récupérer en une ancre dont l'indice est passé en paramètre
+   * @param id L'indice de l'ancre
+   * @return L'ancre
+   */
+  virtual ArRef<Anchor> accessAnchor(size_t id);
 
   AR_CALLBACK(AnchorController, Anchor, _anchorCB, StlVector<ArRef<Anchor> > anchors; )
 
-  virtual
-  void
-  highLightAnchor(ArRef<Anchor> anchor);
+  /**
+   * Méthode permettant de surligné une ancre dans la vue ARéVi
+   * @param anchor L'ancre à surligner
+   */
+  virtual void highLightAnchor(ArRef<Anchor> anchor);
 
-  virtual
-  ArConstRef<Anchor>
-  getHighLightedAnchor(void) const;
+  /**
+   * Méthode permettant de récupérer l'ancre surlignée en consultation 
+   * @return L'ancre surlignée
+   */
+  virtual ArConstRef<Anchor> getHighLightedAnchor(void) const;
 
-  virtual
-  ArRef<Anchor>
-  accessHighLightedAnchor(void);
+  /**
+   * Méthode permettant de récupérer l'ancre surlignée
+   * @return L'ancre surlignée
+   */
+  virtual ArRef<Anchor> accessHighLightedAnchor(void);
 
   AR_CALLBACK(AnchorController, HighLight, _highLightCB,
 	      ArRef<Anchor> anchor;
@@ -67,25 +87,33 @@ public :
 
 protected :
 
-  virtual
-  void
-  _onAnchor(void);
+  /**
+   * Méthode décrivant l'action résultante de la manipulation d'une ancre
+   */
+  virtual void _onAnchor(void);
 
-  virtual
-  void
-  _onHighLight(void);
+  /**
+   * Méthode décrivant l'action résultante du surlignage d'une ancre
+   * @param  
+   */
+  virtual void _onHighLight(void);
 
 protected :
+  /** Vector des ancres du modèle */
   StlVector<ArRef<Anchor> > _anchors;
+  /** Ancre surlignée */
   ArRef<Anchor> _highLighted;
 
+  /** Sphère représentative des ancres */
   ArRef<Sphere3D> _sphere;
+  /** Texture de la sphère */
   ArRef<Material3D> _material;
 
+  /** Gestionnaire de Callback pour les actions sur les ancres */
   CallbackManager<AnchorController, AnchorEvent> _anchorCB;
+  /** Gestionnaire de Callback pour le surlignage des ancres */
   CallbackManager<AnchorController, HighLightEvent> _highLightCB;
 
-  //ArRef<UndoRedo> _undoRedo;
 };
 
 #endif // _ANCHOR_CONTROLLER_H_
